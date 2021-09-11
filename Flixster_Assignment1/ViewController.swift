eada59778680b74b96e5793925d79ac9f5aa2eb3
JuanseMonsalve <56565291+JuanseMonsalve@users.noreply.github.com>
2021-09-11 15:32:35 -0400

    Initial commit

---
+++ Flixster_Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster_Assignment1
+//
+//  Created by Juan S Monsalve on 9/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
