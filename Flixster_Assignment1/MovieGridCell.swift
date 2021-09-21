c15a6dae529cd8f65c7e3bd81338dbcba34c729a
JuanseMonsalve <56565291+JuanseMonsalve@users.noreply.github.com>
2021-09-20 22:41:54 -0400

    Assignment2

---
+++ Flixster_Assignment1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster_Assignment1
+//
+//  Created by Juan S Monsalve on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
